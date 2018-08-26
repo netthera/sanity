5e7b06ec005c2c5c13f695d9af7ed39af3223a84
Nicholas <nicholas.athera@gmail.com>
2018-08-26 14:20:03 +0300

    Create config.php

---
+++ snt_config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$adminUserName = 'adminUserName';//replace with the admin User Name
+if($_SERVER['REMOTE_USER']!=)
+//Array of folders excluded from listing
+$GLOBALS['exclude_folders'] = array(snt_config);
+
+?>
